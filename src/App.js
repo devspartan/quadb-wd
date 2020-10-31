b09f9ab8dca03a2ba1f96bdcdb2ad525f88ae1c2
Devendra Singh <sdev6631@gmail.com>
2020-10-31 21:12:32 +0530

    Add footer and update it's layout

--- src/App.js
+++ src/App.js
@@ -4,14 +4,15 @@ import TopNav from "./Components/Navbar/TopNav";
 import NavbarBanner from "./Components/Navbar/NavbarBanner";
 import Navbar from "./Components/Navbar/Navbar";
 import Content from "./Components/Content/Content";
-import SearchComp from './Components/Navbar/SearchComp'
+import SearchComp from "./Components/Navbar/SearchComp";
 import Navhandle from "./Components/Navbar/Navhandle";
+import Footer from './Components/Footer/Footer'
 function App() {
   return (
-    <div style={{minWidth: "1000px"}}>
-     <Navhandle />
-     <Content />
-
+    <div style={{ minWidth: "1000px" }}>
+      <Navhandle />
+      <Content />
+      <Footer />
     </div>
   );
 }
---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Footer.css";
+import FooterLinkCard from "./FooterLinkCard";
+
+function Footer() {
+  return (
+    <div className="footer-main-container">
+      <div className="footer-wrapper">
+     
+      <FooterLinkCard />
+     
+      <FooterLinkCard />
+      <FooterLinkCard />
+
+      
+
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
