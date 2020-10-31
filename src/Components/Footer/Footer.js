38c2465a0ef20ff4df8f0ea318f3222b0382d932
Devendra Singh <sdev6631@gmail.com>
2020-10-31 22:26:47 +0530

    Add copyright text

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -15,17 +15,24 @@ function Footer() {
   return (
     <div className="footer-main-container">
       <div className="footer-wrapper">
-      <div>
-      <FooterLinkCard data={OtherTimes}/>
-      <FooterLinkCard data={LivingAndEntertainment} />
-      <FooterLinkCard data={TopTrends} />
+        <div>
+          <FooterLinkCard data={OtherTimes} />
+          <FooterLinkCard data={LivingAndEntertainment} />
+          <FooterLinkCard data={TopTrends} />
+        </div>
+        <div>
+          <FooterLinkCard data={Services} />
+          <FooterLinkCard data={PopularCategories} />
+          <FooterLinkCard data={HotOnTheWeb} />
+          <FooterLinkCard data={TrendingTopics} />
+        </div>
       </div>
-      <div>
-      <FooterLinkCard data={Services} />
-      <FooterLinkCard data={PopularCategories} />
-      <FooterLinkCard data={HotOnTheWeb} />
-      <FooterLinkCard data={TrendingTopics} />
-      </div>
+      <div className="footer-copyright">
+        <p>
+          Copyright © 2020 Bennett, Coleman & Co. Ltd. All rights reserved. For
+          reprint rights: 
+        </p>
+        <a> Times Syndication Service</a>
       </div>
     </div>
   );
