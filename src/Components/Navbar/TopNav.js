34c73b14dd715f0ba3ed105cfd223d348550e34e
Devendra Singh <sdev6631@gmail.com>
2020-11-01 01:19:35 +0530

    Add dropdown on topnav for selection of language

--- src/Components/Navbar/TopNav.js
+++ src/Components/Navbar/TopNav.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
 import {
   FaYoutube,
@@ -7,22 +7,45 @@ import {
   BiBell,
   FaCrown,
   FaFacebookF,
+  MdArrowDropDown,
+  MdArrowDropUp,
 } from "react-icons/all";
 
 function TopNav() {
+  const [dropdownActive, setDropdownActive] = useState(false);
+
   return (
     <div className="topnav-main-container">
       <div className="topnav-left">
-        <a>English</a>
-        <a>Epaper</a>
-        <a style={{ borderRight: "none" }}>GadgetsNow</a>
+        <div onMouseEnter={() => setDropdownActive(true)} onMouseLeave={() => setDropdownActive(false)} className='lang-drop-down'>
+          <a className="topnav-left-a" style={{display: "flex"}}>
+            English <MdArrowDropDown className={`drop-down-icon ${dropdownActive ? "drop-down-active" : null}`}/>
+          </a>
+          <div className="topnav-languages-div">
+            <a>हिन्दी</a>
+            <a>मराठी</a>
+            <a>বাংলা</a>
+            <a>ગુજરાતી</a>
+            <a>ಕನ್ನಡ</a>
+            <a style={{paddingTop: "0px", paddingBottom: "0px"}}>தமிழ்</a>
+            <a style={{paddingTop: "0px", paddingBottom: "0px"}}>తెలుగు</a>
+            <a>മലയാളം</a>
+          </div>
+        </div>
+        <a className="topnav-left-a">Epaper</a>
+        <a className="topnav-left-a" style={{ borderRight: "none" }}>GadgetsNow</a>
       </div>
+
       <div className="topnav-right">
         <div>
           <FaCrown style={{ color: "rgb(252,199,57)", fontSize: "18px" }} />
-          <span>Visit TOI dialy & earn TimesPoints!</span>
+          <span>
+            <a>Visit TOI dialy & earn TimesPoints!</a>
+          </span>
         </div>
-        <div>sign in</div>
+        <div>
+          <a>sign in</a>
+        </div>
 
         <div className="topnav-right-icons" style={{ margin: "auto 0" }}>
           <BiBell />
